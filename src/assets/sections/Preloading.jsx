f03eff68bac8e5781bd3717e47776b4a7a41df47
Med Agui <simohamedregragui2@gmail.com>
2024-08-07 16:04:30 +0100

    improving the preloading section

--- src/assets/sections/Preloading.jsx
+++ src/assets/sections/Preloading.jsx
@@ -1,26 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Preloading = () => {
 
+    const loadingRef = useRef(null)
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    const addEventOnElements = (elements, eventType, callback) => {
+        elements.forEach(element => {
+            element.addEventListener(eventType, callback)
+        });
+    }
+
     useEffect(() => {
-        const addEventOnElements = (elements, eventType, callback) => {
-            elements.forEach(element => {
-                element.addEventListener(eventType, callback)
-            });
+
+        const handleLoad = () => {
+            setIsLoaded(true)
+            document.body.classList.remove('active')
         }
 
-        const loadingElement = document.querySelector("[data-loading]")
+        window.addEventListener('load', handleLoad)
 
-        window.addEventListener("load", () => {
-            loadingElement.classList.add("loaded")
-            document.body.classList.remove("active")
-        })
-
+        return () => {
+            window.removeEventListener('load', handleLoad)
+        }
     }, [])
 
   return (
     <div 
-    className='fixed top-0 left-0 w-[100vw] h-[100vh] bg-white z-[5] origin-bottom duration-500 ease-cubic-in delay-500 hidden loading'
+    ref={loadingRef}
+    className={`fixed top-0 left-0 w-[100vw] h-[100vh] bg-white z-[5] origin-bottom duration-500 ease-cubic-in delay-500 loading ${isLoaded ? 'loaded' : 'loading'}`}
     data-loading >
         <img src="icons/loading.svg" alt="loading" width={55} height={55}/>
         <img src="icons/loading-circle.svg" alt="" width={70} height={70} className='circle '/>
